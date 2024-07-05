680f7dbc976eac0e2dd830641dd238316779bbee
rnnunes <ryannunes70@gmail.com>
2024-07-05 19:05:53 -0400

    cmt

---
+++ Caso1/src/main/java/br/com/unimed/Caso1/api/security/SecurityConfig.java
@@ -0,0 +1,13 @@
+package br.com.unimed.Caso1.api.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfiguration {
+
+
+
+}
