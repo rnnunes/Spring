5070a4b0ddf207e8b118f3633701ec7412391e3a
rnnunes <ryannunes70@gmail.com>
2024-07-08 18:56:11 -0400

    cmt

--- Caso1/src/main/java/br/com/unimed/Caso1/api/CarrosController.java
+++ Caso1/src/main/java/br/com/unimed/Caso1/api/CarrosController.java
@@ -8,6 +8,7 @@ import org.springframework.boot.autoconfigure.web.ServerProperties;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -48,6 +49,7 @@ public class CarrosController {
     }
 
     @PostMapping
+    @Secured({ "ROLE_ADMIN" })
     public ResponseEntity post(@RequestBody Carro carro) {
 
         try {
--- Caso1/src/main/java/br/com/unimed/Caso1/api/security/SecurityConfig.java
+++ Caso1/src/main/java/br/com/unimed/Caso1/api/security/SecurityConfig.java
@@ -1,13 +1,50 @@
 package br.com.unimed.Caso1.api.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfiguration {
+@EnableGlobalMethodSecurity(securedEnabled = true)
+public class SecurityConfig {
 
+    @Autowired
+    private DataSource dataSource;
 
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests()
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic();
+        return http.build();
+    }
 
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();       // encoder para criptografar os dados
+
+        User.builder()
+                .username("ADMIN")
+                .password(encoder.encode("123"))
+                .roles("ADM")
+                .build();
+
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                .passwordEncoder(encoder);
+    }
 }
